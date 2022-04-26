d57da7afd15bd8bf520d181bba8b3dfbf2ea327b
Arsham <81646632+arshamalh@users.noreply.github.com>
2022-04-26 21:37:36 +0430

    Create Components/basics/TextArea.svelte

---
+++ Components/basics/TextArea.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let value;
+  export let placeholder;
+  export let cols = "30";
+  export let rows = "20";
+</script>
+
+<textarea {placeholder} bind:value {cols} {rows} />
+
+<style>
+  textarea {
+    border: 2px solid #aaa;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+    font-size: 16px;
+    transition: border 200ms;
+  }
+
+  textarea:focus {
+    outline: none;
+    border: 2px solid rebeccapurple;
+  }
+</style>
