5d9654f7f51704f8902a8b4de728e6b695f52006
Arsham <81646632+arshamalh@users.noreply.github.com>
2022-04-26 21:38:32 +0430

    Create Components/basics/Switch.svelte

---
+++ Components/basics/Switch.svelte
@@ -0,0 +1,53 @@
+<script>
+  export let value = "usernames";
+
+  let checked = true;
+
+  function handleClick(event) {
+    const target = event.target;
+    const state = target.getAttribute("aria-checked");
+    checked = state === "true" ? false : true;
+    value = checked === true ? "usernames" : "ids";
+  }
+</script>
+
+<div class="s s--inner">
+  <button role="switch" aria-checked={checked} on:click={handleClick}>
+    <span>Usernames</span>
+    <span>IDs</span>
+  </button>
+</div>
+
+<style>
+  .s--inner {
+    margin-top: 10px;
+  }
+
+  .s--inner button {
+    background-color: #fff;
+    padding: 1em;
+    border: none;
+    outline: none;
+  }
+
+  [role="switch"][aria-checked="true"] :first-child,
+  [role="switch"][aria-checked="false"] :last-child {
+    display: none;
+    color: #fff;
+  }
+
+  .s--inner button span {
+    user-select: none;
+    pointer-events: none;
+    padding: 0.5em;
+    font-size: 18px;
+  }
+
+  [role="switch"][aria-checked="true"] :first-child,
+  [role="switch"][aria-checked="false"] :last-child {
+    border-radius: 0.25em;
+    background: darkslateblue;
+    display: inline-block;
+    transition: 0.5s;
+  }
+</style>
