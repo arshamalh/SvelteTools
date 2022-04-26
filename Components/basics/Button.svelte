117996ceed0689c627c9f6253bceb3f3d9ab9f4d
Arsham <81646632+arshamalh@users.noreply.github.com>
2022-04-26 21:37:20 +0430

    Create Components/basics/Button.svelte

---
+++ Components/basics/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<button type="submit"><slot /></button>
+
+<style>
+  button {
+    color: #fff;
+    border: none;
+    border-radius: 8px;
+    color: #fff;
+    width: 100px;
+    height: 40px;
+    cursor: pointer;
+    background-color: #202142;
+    transition: 0.2s;
+  }
+
+  button:hover {
+    background-color: #1f3092;
+  }
+</style>
