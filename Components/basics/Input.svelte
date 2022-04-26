b1612c7ee455c2c63ee43d5fc5b917110b1b9bc5
Arsham <81646632+arshamalh@users.noreply.github.com>
2022-04-26 22:38:49 +0430

    Update Components/basics/Input.svelte

--- Components/basics/Input.svelte
+++ Components/basics/Input.svelte
@@ -1,24 +1,24 @@
 <script>
-    export let value;
-    export let placeholder;
-  </script>
-  
-  <input type="text" bind:value {placeholder} />
-  
-  <style>
-    input {
-      flex-grow: 2;
-      border: 2px solid #aaa;
-      padding: 8px 10px;
-      border-radius: 8px;
-      margin-top: 15px;
-      font-size: 16px;
-      transition: border 200ms;
-    }
-  
-    input:focus {
-      outline: none;
-      border: 2px solid rebeccapurple;
-    }
-  </style>
+  export let value;
+  export let placeholder;
+</script>
+
+<input type="text" bind:value {placeholder} />
+
+<style>
+  input {
+    flex-grow: 2;
+    border: 2px solid #aaa;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+    font-size: 16px;
+    transition: border 200ms;
+  }
+
+  input:focus {
+    outline: none;
+    border: 2px solid rebeccapurple;
+  }
+</style>
   
