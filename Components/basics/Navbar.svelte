f3f825a9e906425477dcc31dc5b843fa48af9bb2
Arsham <81646632+arshamalh@users.noreply.github.com>
2022-04-26 21:36:44 +0430

    Create Components/basics/Navbar.svelte

---
+++ Components/basics/Navbar.svelte
@@ -0,0 +1,44 @@
+<script>
+  export let nav_items = [];
+  export let base_href = "";
+  export let menu_selected = 1;
+</script>
+
+<ul>
+  {#each nav_items as item}
+    <li>
+      <a
+        href={base_href}
+        on:click|preventDefault={() => (menu_selected = item.value)}
+        class:active={menu_selected === item.value}>{item.title}</a
+      >
+    </li>
+  {/each}
+</ul>
+
+<style>
+  ul {
+    display: flex;
+    gap: 10px;
+    background-color: #333;
+    flex-wrap: wrap;
+    margin-top: 0;
+  }
+  ul li {
+    display: inline;
+    padding: 15px;
+  }
+
+  ul li a {
+    color: #fff;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+
+  .active {
+    color: rgb(255, 133, 34);
+    font-weight: bolder;
+  }
+</style>
